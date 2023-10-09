9bb3c521d5ee9fda7e2003f01a722a19a28df68a
Vitor Natario <natariovitor@gmail.com>
2023-10-09 19:28:22 -0300

    refactor: add new styleas and new components in Profile

--- src/pages/Home/components/ProfileCard/index.tsx
+++ src/pages/Home/components/ProfileCard/index.tsx
@@ -1,4 +1,9 @@
-import { ProfileCardContainer } from './styles'
+import {
+  ProfileImage,
+  ProfileCardContainer,
+  ProfileDetails,
+  ProfileInfoList,
+} from './styles'
 import { ExternalLink } from '../../../../components/ExternalLink'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
@@ -7,18 +12,17 @@ import { faBuilding, faUserGroup } from '@fortawesome/free-solid-svg-icons'
 export function ProfileCard() {
   return (
     <ProfileCardContainer className="container">
-      <img src="https://github.com/VNatario.png" alt="" />
+      <ProfileImage src="https://github.com/VNatario.png" alt="" />
       <ExternalLink text="github" href="" target="_blank" />
-      <div>
-        <header>
-          <h2>Vitor Natario</h2>
-        </header>
+      <ProfileDetails>
+        <h1>Vitor Natario</h1>
+
         <p>
           Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
           viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat
           pulvinar vel mass.
         </p>
-        <ul>
+        <ProfileInfoList>
           <li>
             <FontAwesomeIcon icon={faGithub} /> VNatario
           </li>
@@ -28,8 +32,8 @@ export function ProfileCard() {
           <li>
             <FontAwesomeIcon icon={faUserGroup} /> 33 seguidores
           </li>
-        </ul>
-      </div>
+        </ProfileInfoList>
+      </ProfileDetails>
     </ProfileCardContainer>
   )
 }
--- src/pages/Home/components/ProfileCard/styles.ts
+++ src/pages/Home/components/ProfileCard/styles.ts
@@ -5,53 +5,55 @@ export const ProfileCardContainer = styled.div`
   border-radius: 10px;
   padding: 2rem;
   display: flex;
+  align-items: center;
   gap: 2rem;
   min-height: 13.25rem;
   box-shadow: 0px 2px 28px 0px rgba(0, 0, 0, 0.2);
   position: relative;
   margin-top: -5.5rem;
 
-  img {
-    height: 9.25rem;
-    width: 9.25rem;
-    border-radius: 8px;
-  }
-
   a {
     position: absolute;
     top: 2.5rem;
     right: 2rem;
   }
+`
 
-  header {
-    justify-content: space-between;
+export const ProfileImage = styled.img`
+  height: 9.25rem;
+  width: 9.25rem;
+  border-radius: 8px;
+`
+
+export const ProfileDetails = styled.div`
+  h1 {
     font-size: 1.5rem;
     font-weight: 700;
     line-height: 1.3;
     color: ${(props) => props.theme['base-title']};
     margin-bottom: 0.5rem;
   }
+`
 
-  ul {
-    list-style: none;
+export const ProfileInfoList = styled.ul`
+  list-style: none;
+  display: flex;
+  gap: 1.5rem;
+  align-items: center;
+  justify-content: flex-start;
+  margin-top: 1.5rem;
+
+  li {
     display: flex;
-    gap: 1.5rem;
     align-items: center;
-    justify-content: flex-start;
-    margin-top: 1.5rem;
+    justify-content: center;
+    gap: 0.5rem;
+    color: ${(props) => props.theme['base-subtitle']};
 
-    li {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      gap: 0.5rem;
-      color: ${(props) => props.theme['base-subtitle']};
-
-      svg {
-        width: 1.125rem;
-        height: 1.125rem;
-        color: ${(props) => props.theme['base-label']};
-      }
+    svg {
+      width: 1.125rem;
+      height: 1.125rem;
+      color: ${(props) => props.theme['base-label']};
     }
   }
 `
