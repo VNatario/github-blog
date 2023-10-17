f14547588bf410ceac462c27f598b9606de68554
Vitor Natario <natariovitor@gmail.com>
2023-10-17 17:44:39 -0300

    feat: add react hook form to form

--- src/pages/Home/components/SearchInput/index.tsx
+++ src/pages/Home/components/SearchInput/index.tsx
@@ -1,15 +1,32 @@
+import { useForm } from 'react-hook-form'
 import { Input } from '../../../../components/Input'
 import { SearchInputContainer } from './styles'
+import * as zod from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const searchFormScheema = zod.object({
+  query: zod.string(),
+})
+
+type SearchFormInput = zod.infer<typeof searchFormScheema>
 
 export function SearchInput() {
+  const { register, handleSubmit } = useForm<SearchFormInput>({
+    resolver: zodResolver(searchFormScheema),
+  })
+
+  function handleSearchPosts(data: SearchFormInput) {
+    console.log(data)
+  }
+
   return (
-    <SearchInputContainer>
+    <SearchInputContainer onSubmit={handleSubmit(handleSearchPosts)}>
       <div>
         <h2>Publicações</h2>
         <span>6 publicações</span>
       </div>
 
-      <Input placeholder="Buscar conteúdo" />
+      <Input placeholder="Buscar conteúdo" {...register('query')} />
     </SearchInputContainer>
   )
 }
