5c0ffaa6c968fb6a8a5b3c12ffa104f2ec8586a0
Vitor Natario <natariovitor@gmail.com>
2023-10-17 21:10:13 -0300

    feat: add dinamics data to ProfileCard

--- src/pages/Home/components/ProfileCard/index.tsx
+++ src/pages/Home/components/ProfileCard/index.tsx
@@ -8,29 +8,59 @@ import { ExternalLink } from '../../../../components/ExternalLink'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faBuilding, faUserGroup } from '@fortawesome/free-solid-svg-icons'
+import { useCallback, useEffect, useState } from 'react'
+import { api } from '../../../../lib/axios'
+
+const username = import.meta.env.VITE_GITHUB_USERNAME
+
+interface ProfileData {
+  login: string
+  bio: string
+  avatar_url: string
+  html_url: string
+  name: string
+  company?: string
+  followers: number
+}
 
 export function ProfileCard() {
+  const [profileData, setProfileData] = useState<ProfileData>({} as ProfileData)
+  const [isLoading, setIsLoading] = useState(true)
+
+  const getProfileData = useCallback(async () => {
+    try {
+      setIsLoading(true)
+      const response = await api.get(`/users/${username}`)
+      setProfileData(response.data)
+    } finally {
+      setIsLoading(false)
+    }
+  }, [profileData])
+
+  useEffect(() => {
+    getProfileData()
+  }, [])
+
   return (
     <ProfileCardContainer className="container">
-      <ProfileImage src="https://github.com/VNatario.png" alt="" />
-      <ExternalLink text="github" href="" target="_blank" />
+      <ProfileImage src={profileData.avatar_url} alt="" />
+      <ExternalLink text="github" href={profileData.html_url} target="_blank" />
       <ProfileDetails>
-        <h1>Vitor Natario</h1>
+        <h1>{profileData.name}</h1>
 
-        <p>
-          Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
-          viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat
-          pulvinar vel mass.
-        </p>
+        <p>{profileData.bio}</p>
         <ProfileInfoList>
           <li>
-            <FontAwesomeIcon icon={faGithub} /> VNatario
+            <FontAwesomeIcon icon={faGithub} /> {profileData.login}
           </li>
+          {profileData?.company && (
+            <li>
+              <FontAwesomeIcon icon={faBuilding} /> {profileData.company}
+            </li>
+          )}
           <li>
-            <FontAwesomeIcon icon={faBuilding} /> None
-          </li>
-          <li>
-            <FontAwesomeIcon icon={faUserGroup} /> 33 seguidores
+            <FontAwesomeIcon icon={faUserGroup} /> {profileData.followers}{' '}
+            seguidores
           </li>
         </ProfileInfoList>
       </ProfileDetails>
